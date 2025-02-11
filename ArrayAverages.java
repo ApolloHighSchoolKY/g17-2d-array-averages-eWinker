d05c8df1d43676ea068b78af2354c4714da2d227
eWinker <elijah.winker@stu.daviess.kyschools.us>
2025-02-11 15:57:29 +0000

    hi

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -9,15 +9,38 @@ public class ArrayAverages{
   }
 
   public void rowAverages(){
-    
+    double avg=0;
+    for(int row=0; row<ray.length; row++){
+      avg=0;
+      for(int col=0; col<ray[row].length; col++){
+        avg+=ray[row][col];
+      }
+      avg = avg / ray[row].length;
+      System.out.println(avg);
+    }
   }
 
   public void columnAverages(){
-
+    double avg=0;
+    for(int col=0; col<ray[0].length; col++){
+      avg=0;
+      for(int row=0; row<ray.length; row++){
+        avg+=ray[row][col];
+      }
+      avg = avg / ray[col].length;
+      System.out.println(avg);
+    }
   }
 
   public void arrayAverage(){
-
+    double avg=0;
+    for(int row=0; row<ray.length; row++){
+      for(int col=0; col<ray[row].length; col++){
+        avg+=ray[row][col];
+      }
+    }
+     avg = avg / ray[ray.length-1][ray[0].length-1];
+    System.out.println(avg);
   }
 
   public String toString(){
@@ -25,5 +48,7 @@ public class ArrayAverages{
     {
       System.out.println(Arrays.toString(ray[row]));
     }
+
+    return"";
   }
 }
